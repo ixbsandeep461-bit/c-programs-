fc09277c1090d3382879260dc73d9b740a08c54d
Sandeep R <ixbsandeep461@gmail.com>
2026-07-14 20:07:08 +0530

    Create find_numbers_with_even_digits.c

---
+++ find_numbers_with_even_digits.c
@@ -0,0 +1,20 @@
+int findNumbers(int* nums, int numsSize) {
+    int i;
+    int evenCount = 0;
+
+    for(i = 0; i < numsSize; i++) {
+        int temp = nums[i];
+        int digitCount = 0;
+
+        while(temp != 0) {
+            digitCount++;
+            temp = temp / 10;
+        }
+
+        if(digitCount % 2 == 0) {
+            evenCount++;
+        }
+    }
+
+    return evenCount;
+}
